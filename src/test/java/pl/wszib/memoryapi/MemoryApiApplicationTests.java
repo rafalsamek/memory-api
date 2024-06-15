ffc7354b813862fba39feccf836c14fff1b57e6c
rsamek <rafal.samek@gmail.com>
2024-06-15 11:33:52 +0200

    Initial commit

---
+++ src/test/java/pl/wszib/memoryapi/MemoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.memoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
