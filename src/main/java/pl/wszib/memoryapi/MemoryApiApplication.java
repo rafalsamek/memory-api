b716bbbd51b8be4088eeb4d54c9ff047f361d958
Sebastian Słomian <slomian@wszib.edu.pl>
2024-06-15 11:30:50 +0200

    Init

---
+++ src/main/java/pl/wszib/memoryapi/MemoryApiApplication.java
@@ -0,0 +1,13 @@
+package pl.wszib.memoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryApiApplication.class, args);
+    }
+
+}
